2c40218a19758879e01e8abd894441698b22b4e8
arp101 <arpit.raorane@gmail.com>
2016-04-07 00:31:06 -0500

    Update p1_adr9202.s

--- p1_adr9202.s
+++ p1_adr9202.s
@@ -78,13 +78,15 @@ _maximum:
 
 
 _scanf:
+    MOV R9, LR
     SUB SP, SP, #4          @ make room on stack
     LDR R0, =format_str     @ R0 contains address of format string
     MOV R1, SP              @ move SP to R1 to store entry on stack
     BL scanf                @ call scanf
     LDR R0, [SP]            @ load value at SP into R0
     ADD SP, SP, #4          @ restore the stack pointer
-    MOV PC, LR              @ return
+    MOV PC, R9
+    
 
 
 _scanf2:
