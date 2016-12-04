15a92bfd87f906362426bc87552e7ac70cbcb6fa
arp101 <arpit.raorane@gmail.com>
2016-12-04 13:07:13 -0600

    Create Lab4.c

---
+++ Lab4.c
@@ -0,0 +1,14 @@
+
+
+/**************************HEADERS********************************************/
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "Lab4Func.h"
+
+/************************MAIN*************************************************/
+
+int main(){
+  menu();
+ return EXIT_SUCCESS; 
+}
